b76dc60f8e7471bedb3b12a6ee9480254e38f84f
Sleep-pig <1584736136@qq.com>
2025-02-05 22:51:47 +0800

    实现无边框缩放,存在一些bug 如暂停时窗口变化时画面不变化以及缩放时边界判断在窗口内和相应鼠标指针没变

--- src/UI/core/MediaPlayer.cpp
+++ src/UI/core/MediaPlayer.cpp
@@ -5,12 +5,13 @@
 #include <QHBoxLayout>
 #include <QLineEdit>
 #include <qmainwindow.h>
+#include <qnamespace.h>
 #include <QPushButton>
 #include <QStandardPaths>
-#include <qnamespace.h>
 #include <qwidget.h>
 
 MediaPlayer::MediaPlayer(QWidget *parent) : QMainWindow(parent) {
+    setAttribute(Qt::WA_Hover);
     setWindowFlags(Qt::FramelessWindowHint);
     w = new MediaDialog(this);
     setCentralWidget(w);
@@ -24,33 +25,113 @@ MediaPlayer::MediaPlayer(QWidget *parent) : QMainWindow(parent) {
             this, &MediaPlayer::showMaximized);
 
     connect(w, &MediaDialog::startDragging, this,
-            &MediaPlayer::handleStartDragging,Qt::DirectConnection);
-    connect(w, &MediaDialog::dragging, this, &MediaPlayer::handleDragging,Qt::DirectConnection);
-
-    // w->installEventFilter(this);
+            &MediaPlayer::handleStartDragging, Qt::DirectConnection);
+    connect(w, &MediaDialog::dragging, this, &MediaPlayer::handleDragging,
+            Qt::DirectConnection);
 }
 
-// bool MediaPlayer::eventFilter(QObject *watched, QEvent *eve) {
-//     QMouseEvent *event = static_cast<QMouseEvent *>(eve);
-//     if (watched == w) {
-//         if (event->pos().y() <= w->controlWidget->titlebar->height()) {
-//             if (event->type() == QEvent::MouseButtonPress) {
-//                 if (event->button() == Qt::LeftButton) {
-//                     mousePressed = true;
-//                     mousePoint = event->globalPos() - this->pos();
-//                     return true;
-//                 }
-//             } else if (event->type() == QEvent::MouseButtonRelease) {
-//                 mousePressed = false;
-//                 return true;
-//             } else if (event->type() == QEvent::MouseMove) {
-//                 MediaPlayer::move(event->globalPos() - mousePoint);
-//                 return true;
-//             }
-//         }
-//     }
-
-//     return QWidget::eventFilter(watched, eve);
-// }
-
 MediaPlayer::~MediaPlayer() {}
+
+Qt::Edges MediaPlayer::calculateEdge(QPoint const &pos) {
+    Qt::Edges edges;
+    int x = pos.x();
+    int y = pos.y();
+    int w = width();
+    int h = height();
+
+    if (x < m_borderWidth) {
+        edges |= Qt::LeftEdge;
+    }
+    if (x > w - m_borderWidth) {
+        edges |= Qt::RightEdge;
+    }
+    if (y < m_borderWidth) {
+        edges |= Qt::TopEdge;
+    }
+    if (y > h - m_borderWidth) {
+        edges |= Qt::BottomEdge;
+    }
+
+    if (x < m_borderWidth && y < m_borderWidth) {
+        edges |= Qt::LeftEdge | Qt::TopEdge;
+    }
+    if (x > w - m_borderWidth && y < m_borderWidth) {
+        edges |= Qt::RightEdge | Qt::TopEdge;
+    }
+    if (x < m_borderWidth && y > h - m_borderWidth) {
+        edges |= Qt::LeftEdge | Qt::BottomEdge;
+    }
+    if (x > w - m_borderWidth && y > h - m_borderWidth) {
+        edges |= Qt::RightEdge | Qt::BottomEdge;
+    }
+    return edges;
+}
+
+bool MediaPlayer::ConfirmEdge(Qt::Edges &edge) {
+    if (edge == Qt::LeftEdge || edge == Qt::RightEdge || edge == Qt::TopEdge ||
+        edge == Qt::BottomEdge || (edge == Qt::LeftEdge | Qt::TopEdge) ||
+        (edge == Qt::RightEdge | Qt::TopEdge) ||
+        (edge == Qt::LeftEdge | Qt::BottomEdge) ||
+        (edge == Qt::RightEdge | Qt::BottomEdge)) {
+        return true;
+    }
+    return false;
+}
+
+void MediaPlayer::mousePressEvent(QMouseEvent *event) {
+    if (event->button() == Qt::LeftButton) {
+        m_dragStartPosition = event->globalPos();
+        m_dragEdge = calculateEdge(m_dragStartPosition);
+        m_isDragging = ConfirmEdge(m_dragEdge);
+    }
+    QWidget::mousePressEvent(event);
+}
+
+void MediaPlayer::mouseMoveEvent(QMouseEvent *event) {
+    if (m_isDragging) {
+        // 拖拽调整窗口大小
+        QPoint delta = event->globalPos() - m_dragStartPosition;
+        QRect newGeometry = geometry();
+
+        if (m_dragEdge & Qt::LeftEdge) {
+            newGeometry.setLeft(newGeometry.left() + delta.x());
+        }
+        if (m_dragEdge & Qt::RightEdge) {
+            newGeometry.setRight(newGeometry.right() + delta.x());
+        }
+        if (m_dragEdge & Qt::TopEdge) {
+            newGeometry.setTop(newGeometry.top() + delta.y());
+        }
+        if (m_dragEdge & Qt::BottomEdge) {
+            newGeometry.setBottom(newGeometry.bottom() + delta.y());
+        }
+
+        setGeometry(newGeometry);
+        m_dragStartPosition = event->globalPos();
+    } else {
+        // 更新光标形状
+        Qt::Edges edge = calculateEdge(event->pos());
+        updateCursor(edge);
+    }
+    QWidget::mouseMoveEvent(event);
+}
+
+void MediaPlayer::mouseReleaseEvent(QMouseEvent *event) {
+    m_isDragging = false;
+    m_dragEdge = Qt::Edges();
+    QWidget::mouseReleaseEvent(event);
+}
+
+void MediaPlayer::updateCursor(Qt::Edges& edges) {
+    if (edges == (Qt::LeftEdge | Qt::TopEdge) || edges == (Qt::RightEdge | Qt::BottomEdge)) {
+        setCursor(Qt::SizeFDiagCursor);
+    } else if (edges == (Qt::RightEdge | Qt::TopEdge) || edges == (Qt::LeftEdge | Qt::BottomEdge)) {
+        setCursor(Qt::SizeBDiagCursor);
+    } else if (edges & (Qt::LeftEdge | Qt::RightEdge)) {
+        setCursor(Qt::SizeHorCursor);
+    } else if (edges & (Qt::TopEdge | Qt::BottomEdge)) {
+        setCursor(Qt::SizeVerCursor);
+    } else {
+        unsetCursor();
+    }
+}
--- src/UI/include/MediaPlayer.hpp
+++ src/UI/include/MediaPlayer.hpp
@@ -2,6 +2,7 @@
 #include "MediaDialog.hpp"
 #include <qglobal.h>
 #include <qmainwindow.h>
+#include <qnamespace.h>
 #include <qobjectdefs.h>
 #include <qpoint.h>
 #include <QWidget>
@@ -12,11 +13,17 @@ class MediaPlayer : public QMainWindow {
 public:
     MediaPlayer(QWidget *parent = nullptr);
     ~MediaPlayer();
+    Qt::Edges calculateEdge(QPoint const &pos);
+    bool ConfirmEdge(Qt::Edges &edge);
+    void updateCursor(Qt::Edges &edges);
 
-    // protected:
-    //     bool eventFilter(QObject *obj, QEvent *event) override;
+protected:
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
 
 private slots:
+
     void handleStartDragging(QPoint offset) {
         dragOffset = offset;
     }
@@ -27,6 +34,11 @@ private slots:
 
 private:
     QPoint dragOffset;
+
+    bool m_isDragging{false};
+    QPoint m_dragStartPosition;
+    int m_borderWidth{3};
+    Qt::Edges m_dragEdge = Qt::Edges();
 
 private:
     MediaDialog *w{nullptr};
